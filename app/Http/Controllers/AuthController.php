abdeaebaae04dc1094b639e015be39ab937de265
Mateus Gamba <mateusgamba@gmail.com>
2020-11-16 22:43:47 -0300

    feat: authentication and register

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Str;
+use Laravel\Passport\Client as OClient;
+
+class AuthController extends Controller
+{
+    /**
+     * @param  Request  $request
+     * @return JsonResponse
+     */
+    public function register(Request $request): JsonResponse
+    {
+        $request['remember_token'] = Str::random(10);
+        $request['password'] = Hash::make($request['password']);
+
+        $user = User::create($request->toArray());
+
+        return response()->json($user, 201);
+    }
+
+    /**
+     * @param  Request  $request
+     * @return JsonResponse
+     */
+    public function login(Request $request): JsonResponse
+    {
+        $login = Arr::only($request->toArray(), ['email', 'password']);
+
+        if (!Auth::attempt($login)) {
+            return response()->json([ 'error' => 'Unauthorised' ], 401);
+        }
+
+        $objToken = $this->getAccessToken($request);
+
+        return response()->json($objToken, 200);
+    }
+
+    /**
+     * @param  Request  $request
+     * @return JsonResponse
+     */
+    public function logout(Request $request): JsonResponse
+    {
+        $request->user()->token()->revoke();
+
+        return response()->json([
+            'message' => 'Successfully logged out'
+        ]);
+    }
+
+    /**
+     * @return JsonResponse
+     */
+    public function me(): JsonResponse
+    {
+        $user = Auth::user();
+        return response()->json($user, 200);
+    }
+
+    /**
+     * @return JsonResponse
+     */
+    public function unauthorized(): JsonResponse
+    {
+        return response()->json([ 'error' => 'Unauthorized' ], 401);
+    }
+
+    /**
+     * @return array
+     */
+    public function getAccessToken(Request $request): array
+    {
+        $oClient = OClient::where('password_client', 1)->first();
+
+        $response = Http::post(env('OAUTH_URL'), [
+            'grant_type' => 'password',
+            'client_id' => $oClient->id,
+            'client_secret' => $oClient->secret,
+            'username' => $request->email,
+            'password' => $request->password,
+            'scope' => '',
+        ]);
+
+        return json_decode((string) $response->getBody(), true);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PostController;
 
 /*
@@ -20,8 +21,19 @@ Route::get('/', function () {
     ]);
 });
 
+Route::post('register', [ AuthController::class, 'register' ]);
+Route::post('login', [ AuthController::class, 'login' ]);
+
 Route::get('posts', [ PostController::class, 'index' ]);
 Route::get('posts/{post}', [ PostController::class, 'show' ]);
-Route::post('posts', [ PostController::class, 'store' ]);
-Route::put('posts/{post}', [ PostController::class, 'update' ]);
-Route::delete('posts/{post}', [ PostController::class, 'destroy' ]);
+
+Route::get('unauthorized', [ AuthController::class, 'unauthorized' ]);
+
+Route::group(['middleware' => ['auth:api']], function() {
+    Route::post('logout', [ AuthController::class, 'logout' ]);
+    Route::get('me', [ AuthController::class, 'me' ]);
+
+    Route::post('posts', [ PostController::class, 'store' ]);
+    Route::put('posts/{post}', [ PostController::class, 'update' ]);
+    Route::delete('posts/{post}', [ PostController::class, 'destroy' ]);
+});
